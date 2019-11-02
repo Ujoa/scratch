42622bb069bc179b97dc862e540a79c72086b2e7
Juan Parra <41169130+Ujoa@users.noreply.github.com>
2019-11-02 14:32:32 -0400

    events

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from './Header';
 import Footer from './Footer';
-import Login from './LoginPage';
+
 import './styles/App.css';
 
 
@@ -13,7 +13,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to the College Events Website!</h1>
       </header>
-      <Login />
+      
       <Footer />  
     </div>
   );
